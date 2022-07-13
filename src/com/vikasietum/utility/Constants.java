2ee996ee8eb74b477a9b5cd36003b68450da923f
ankittiwary06 <ankittiwary06@gmail.com>
2022-07-13 13:28:01 +0530

    AT - added utility class message

--- src/com/vikasietum/utility/Constants.java
+++ src/com/vikasietum/utility/Constants.java
@@ -3,5 +3,8 @@ package com.vikasietum.utility;
 public class Constants {
     public static final String FILEPATH="C:\\Users\\neha\\Desktop\\vikasietum\\addressbook\\";
     public static final String NO_CONTACT_FOUND="No contacts found!";
+    public static final String CONTACT_UPDATE_MESSAGE="your contact has been updated";
+    public static final String CONTACT_REMOVE_MESSAGE="Given contact has been deleted!";
+    public static final String CONTACT_SAVED_MESSAGE="contact has been saved";
 
 }
